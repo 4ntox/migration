acb41cacead8fd64fe29f1a3645e9ea492a97a72
Antox Hooligan <90599264+4ntox@users.noreply.github.com>
2021-11-09 21:07:50 +0700

    migration
    migration

---
+++ 2021_11_09_134527_create_guru_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGuruTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('guru', function (Blueprint $table) {
+            $table->string('nik',20)->primary();
+            $table->string('nama',20);
+            $table->string('jk',1);
+            $table->text('alamat')->nullable();
+            $table->string('tmp_lahir',50);
+            $table->date('tgl_lahir');
+            $table->string('telepon',20)->nullable();
+            $table->integer('jurusan',11);
+            $table->integer('nilai',11)->nullable();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('guru');
+    }
+}
